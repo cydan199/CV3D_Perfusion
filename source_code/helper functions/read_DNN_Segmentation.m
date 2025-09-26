69df823678dacc7a5da3c56cd2ec79c4f72a1a27
cydan199 <chen_yudan@yahoo.com>
2025-09-26 13:59:34 -0700

    remove source

--- source_code/helper functions/read_DNN_Segmentation.m
+++
@@ -1,9 +0,0 @@
-function data_permute = read_DNN_Segmentation(loadloc)
-
-% Loading Volume
-data = niftiread(loadloc);
-
-data_permute = permute(data,[2,1,3]);
-data_permute_flip = flipdim(data_permute,1);
-
-end
--- source_code/helper functions/saveAsIMG.m
+++
@@ -1,5 +0,0 @@
-function saveAsIMG(filename, data)
-    fid = fopen(filename,'w');
-    fwrite(fid, data, 'uint8');
-    fclose(fid);
-end
